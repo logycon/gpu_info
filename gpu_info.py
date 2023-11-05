13eb4c4975a93b58d199d768279730a8cfd7fbe5
logycon <igor@logycon.com>
2023-11-05 08:11:43 -0500

    Add error handling

--- gpu_info.py
+++ gpu_info.py
@@ -4,24 +4,31 @@ import json
 def print_gpu_info():
     gpu_info = []
 
-    pynvml.nvmlInit()
-    device_count = pynvml.nvmlDeviceGetCount()
+    try:
+        pynvml.nvmlInit()
+        device_count = pynvml.nvmlDeviceGetCount()
 
-    for i in range(device_count):
-        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
-        gpu_name = pynvml.nvmlDeviceGetName(handle)
-        gpu_memory = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        for i in range(device_count):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+            gpu_name = pynvml.nvmlDeviceGetName(handle)
+            gpu_memory = pynvml.nvmlDeviceGetMemoryInfo(handle)
+            gpu_info.append({
+                'gpu': i + 1,
+                'name': gpu_name,
+                'memory': {
+                    'used_mb': gpu_memory.used / 1024**2,
+                    'total_mb': gpu_memory.total / 1024**2
+                }
+            })
+        pynvml.nvmlShutdown()
+    except pynvml.NVMLError as error:
         gpu_info.append({
-            'gpu': i + 1,
-            'name': gpu_name,
-            'memory': {
-                'used_mb': gpu_memory.used / 1024**2,
-                'total_mb': gpu_memory.total / 1024**2
-            }
+            'error': error.__str__(),
         })
-    pynvml.nvmlShutdown()
+    
     json_str = json.dumps(gpu_info, indent = 4)
     print(json_str)
+
 
 
 if __name__ == '__main__':
